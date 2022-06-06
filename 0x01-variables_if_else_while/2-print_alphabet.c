7b422789df66af140928675f81a60b8d06cdba30
EdidiongMfon <nipzycool@gmail.com>
2022-06-06 13:32:25 -0700

    1-5

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ ** main - Entry point
+ ** Return: Always 0 (Success)
+ **/
+int main (void)
+{
+char lc;
+for (lc = 'a'; lc <= 'z'; lc++)
+putchar(lc);
+return (0);
+}
