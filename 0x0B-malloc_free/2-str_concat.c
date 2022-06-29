8b394e38a7f75eb4270207a3054d409f5fd3aeab
EdidiongMfon <nipzycool23@gmail.com>
2022-06-28 19:54:43 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,8 +11,10 @@ char *str_concat(char *s1, char *s2)
 {
 int i = 0, j = 0, l = 0, k = 0;
 char *array;
-if (s1 == NULL || s2 == NULL)
-s1 = s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 while (s1[i])
 i++;
 while (s2[j])
