a9006ef57b615f8d3b9c7721a01af99f18714646
EdidiongMfon <nipzycool23@gmail.com>
2022-07-10 07:51:13 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  ** main - Entry point
+ **
  ** Return: Always 0 (Success)
  **/
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61)
+putchar(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 return (1);
 }
